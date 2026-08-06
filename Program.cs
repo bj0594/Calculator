50b5edd2eaa4553f2a3779da3a0c136ef2c27d31
bj0594 <bj0594.kode@gmail.com>
2026-08-06 14:48:51 +0200

    added a simple numpad overview and a column and row counter

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int row = 0;
+int column = 0;
+
+Console.WriteLine("7 8 9 /\n4 5 6 *\n1 2 3 -\n[0] . = +");
+
+while (true)
+{
+    ConsoleKeyInfo input = Console.ReadKey();
+
+    if (input.Key == ConsoleKey.Escape)
+    {
+        break;
+    }
+
+    switch (input.Key)
+    {
+        case ConsoleKey.UpArrow:
+        row--;
+        break;
+
+        case ConsoleKey.DownArrow:
+        row++;
+        break;
+
+        case ConsoleKey.LeftArrow:
+        column--;
+        break;
+
+        case ConsoleKey.RightArrow:
+        column++;
+        break;
+    }
+}
+Console.WriteLine($"Row: {row}, Column: {column}");
