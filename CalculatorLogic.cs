7d9231bba3414d7db2d2ee2a6b0f462eaaadcb46
bj0594 <bj0594.kode@gmail.com>
2026-08-10 14:11:42 +0200

    restructured calculatorlogic so overloads could handle more expressions

--- CalculatorLogic.cs
+++ CalculatorLogic.cs
@@ -1,42 +1,85 @@
 public static class CalculatorLogic
 {
-    // This method takes a string expression in the format "number1 operator number2"
-    // and performs the calculation based on the operator.
+    // Receives the expression from Program.cs
+    // and prepares the values for the calculation.
     public static string Calculate(string expression)
     {
-        // Find the index of the operator in the expression
+        // Find the first operator
         int operatorIndex = expression.IndexOfAny(['+', '-', '*', '/']);
 
         // If no operator is found, return the original expression
         if (operatorIndex == -1)
         {
-            return expression; 
+            return expression;
         }
 
-        // Split the expression into two numbers based on the operator index
-        string firstNumber = expression[..operatorIndex];
-        string secondNumber = expression[(operatorIndex + 1)..];
+        // Find the operator
+        char operation = expression[operatorIndex];
 
-        double first = double.Parse(firstNumber);
-        double second = double.Parse(secondNumber);
+        // Split the expression into numbers
+        string[] numberStrings = expression.Split(operation);
 
-        // Perform the calculation based on the operator
-        switch (expression[operatorIndex])
+        // Convert the numbers from strings to doubles
+        List<double> numbers = new List<double>();
+
+        foreach (string number in numberStrings)
+        {
+            numbers.Add(double.Parse(number));
+        }
+
+        // If there are only two numbers, use the two-number overload
+        if (numbers.Count == 2)
+        {
+            double result = Calculate(numbers[0], numbers[1], operation);
+            return result.ToString("0.##");
+        }
+
+        // If there are more than two numbers, use the list overload
+        double listResult = Calculate(numbers, operation);
+        return listResult.ToString("0.##");
+    }
+
+
+    // Overload 1:
+    // Calculates an operation between two numbers.
+    public static double Calculate(double first, double second, char operation)
+    {
+        switch (operation)
         {
             case '+':
-                return (first + second).ToString();
+                return first + second;
+
             case '-':
-                return (first - second).ToString();
+                return first - second;
+
             case '*':
-                return (first * second).ToString();
+                return first * second;
+
             case '/':
                 if (second == 0)
                 {
-                    return "Error";
+                    throw new DivideByZeroException();
                 }
-                return (first / second).ToString("0.##"); // Format to 2 decimal places
+
+                return first / second;
+
             default:
-                return expression; // Unknown operator, return the original expression
+                throw new ArgumentException("Unknown operator");
         }
+    }
+
+
+    // Overload 2:
+    // Calculates an operation using multiple numbers.
+    public static double Calculate(List<double> numbers, char operation)
+    {
+        double result = numbers[0];
+
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            result = Calculate(result, numbers[i], operation);
+        }
+
+        return result;
     }
 }
